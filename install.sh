117627aeff3b0e635896626d941dafee3e1aeaf9
Austen Stone <austenstone@github.com>
2023-04-06 10:58:53 -0400

    symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
 # OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd "${HOME}/.oh-my-zsh"; git pull; exec -l zsh
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+create_symlinks
