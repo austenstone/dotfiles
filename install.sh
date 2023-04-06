750b1d2dc39b4db404ffa95ece240c730cde1e20
Austen Stone <austenstone@github.com>
2023-04-06 10:55:28 -0400

    test

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd "$ZSH"; git pull; exec -l zsh
+cd "${HOME}/.oh-my-zsh"; git pull; exec -l zsh
